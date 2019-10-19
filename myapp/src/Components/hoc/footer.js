398bcf7b36390358b76738f211f91e1407d61c11
saumyajeet1 <kol.kuheli@gmail.com>
2019-10-20 02:06:02 +0530

    footer

--- myapp/src/Components/hoc/footer.js
+++ myapp/src/Components/hoc/footer.js
@@ -2,15 +2,37 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className="row">
-            <div>
-                For more details contact us at
+        <div className="row footer">
+            <div className="row">
+            <div className="col-lg-6 email">
+                <div className="row">
+                    
+                <div className=" col-lg-2 title"></div>
+                <div className=" col-lg-5 title">Email:-</div>
+                <div className="col-lg-5 info">xxxxx@gmail.com</div>
+                </div>
             </div>
-            <div className="tex">
-                EMAIL:""
+            <div className="col-lg-6 contact">
+                <div className="row">
+                <div className=" col-lg-2 title"></div>
+                <div className=" col-lg-5 title">Contact:-</div>
+                <div className="col-lg-5 info">1111111111</div>
+                </div>
             </div>
-            <div className="tex">
-                PHONE NUMBER:""
+            </div>
+            <div className="row">
+            <div className="col-lg-6 title">
+                <div className="row">
+                <div className=" col-lg-2 title"></div>
+                <div className="col-lg-10 title">SPICMACAY</div>
+                </div>
+            </div>
+            <div className="col-lg-6 title">
+                <div className="row">
+                <div className=" col-lg-2 title"></div>
+                <div className=" col-lg-10 title">An MHRD Club</div>
+                </div>
+            </div>
             </div>
         </div>
     );
