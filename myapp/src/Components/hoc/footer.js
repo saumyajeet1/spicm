7a559ba766342aa9a3859d80a9723805d2ace198
saumyajeet1 <kol.kuheli@gmail.com>
2019-11-06 12:06:35 +0530

    changes

--- myapp/src/Components/hoc/footer.js
+++ myapp/src/Components/hoc/footer.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faPhone from '@fortawesome/fontawesome-free-solid/faPhone';
+import faEnvelope from '@fortawesome/fontawesome-free-solid/faEnvelope';
+import {faFacebook} from '@fortawesome/free-brands-svg-icons';
+import {faInstagram} from '@fortawesome/free-brands-svg-icons';
+  
+
 const Footer = () => {
     return (
         <div className="row footer" style={{background:"black",opacity:"0.7"}}>
@@ -8,14 +15,39 @@ const Footer = () => {
                 <div className="row">
                     
                 <div className=" col-lg-2 title"></div>
-                <div className=" col-lg-5 title"><p className="des">Email:-</p></div>
+                <div className=" col-lg-5 title"><p className="des"><FontAwesomeIcon icon={faEnvelope} className="icon"/>Email:-</p></div>
                 <div className="col-lg-5 info"><p className="des">xxxxx@gmail.com</p></div>
                 </div>
             </div>
             <div className="col-lg-6 contact">
                 <div className="row">
                 <div className=" col-lg-2 title"></div>
-                <div className=" col-lg-5 title"><p  className="des">Contact:-</p></div>
+                <div className=" col-lg-5 title"><p  className="des" style={{fontFamily:"algerian",fontSize:"25"}}> <FontAwesomeIcon
+                                    icon={faPhone}
+                                    className="icon"
+                                />
+                                Contact:-</p></div>
+                <div className="col-lg-5 info"><p className="des">1111111111</p></div>
+                </div>
+            </div>
+            </div>
+            <div className="row">
+            <div className="col-lg-6 email">
+                <div className="row">
+                    
+                <div className=" col-lg-2 title"></div>
+                <div className=" col-lg-5 title"><p className="des"><FontAwesomeIcon icon={faInstagram} className="icon"/>Email:-</p></div>
+                <div className="col-lg-5 info"><p className="des">xxxxx@gmail.com</p></div>
+                </div>
+            </div>
+            <div className="col-lg-6 contact">
+                <div className="row">
+                <div className=" col-lg-2 title"></div>
+                <div className=" col-lg-5 title"><p  className="des"> <FontAwesomeIcon
+                                    icon={faFacebook}
+                                    className="icon"
+                                />
+                                Contact:-</p></div>
                 <div className="col-lg-5 info"><p className="des">1111111111</p></div>
                 </div>
             </div>
