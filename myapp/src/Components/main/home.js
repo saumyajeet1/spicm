2a5ba80ec0ec649fc29724dc30910f89b2790f1d
saumyajeet1 <kol.kuheli@gmail.com>
2019-11-06 07:28:38 +0530

    Update home.js

--- myapp/src/Components/main/home.js
+++ myapp/src/Components/main/home.js
@@ -14,7 +14,7 @@ class Home extends Component {
                     
                 <Des/>
                 <My/>
-                   <Team/>
+                <Team/>
                    
                </div>
         );
